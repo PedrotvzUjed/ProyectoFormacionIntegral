5a0ab5466be0567207d2b8343b93ff4124b2134b
JesusSantos99 <jesus.santos@ujed.mx>
2021-10-24 19:13:20 -0500

    Componente de informacion de evento

---
+++ projectpfi/pfi-front/src/components/Eventos/InfoEvento.vue
@@ -0,0 +1,132 @@
+<template>
+  <v-card v-if="evento.tituloEvento != ''"
+    class=""
+  >
+    <v-img
+      height="150"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+    ></v-img>
+
+    <v-card-title>{{ evento.tituloEvento }}</v-card-title>
+
+    <v-card-text>
+      <v-row>
+        <label><strong>Descripcion del evento:</strong></label>
+        <v-spacer></v-spacer>
+        <label>{{ evento.descripcionEvento }}</label>
+      </v-row>
+      <v-row>
+        <label><strong>Unidad Responsable del evento: </strong></label>
+        <v-spacer></v-spacer>
+        <label>{{ evento.unidadResponsable }}</label>
+      </v-row>
+      <v-row>
+        <label><strong>Evento dedicado a: </strong></label>
+        <v-spacer></v-spacer>
+        <label>{{ evento.eventoDedicadoA }}</label>
+      </v-row>
+      <v-row>
+        <v-col>
+          <label><strong>Fecha del evento:</strong></label>
+        </v-col>
+        <v-col>
+          <label> {{ evento.fechaEvento }}</label>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <label><strong>Hora de inicio del evento: </strong>{{ evento.inicioEvento }}</label>
+        </v-col>
+        <v-col>
+          <label><strong>Hora del final del evento: </strong> {{ evento.finEvento }} </label>
+        </v-col>
+      </v-row>
+      <v-row>
+        <label><strong>Sede del evento:</strong></label>
+        <v-spacer></v-spacer>
+        <label>{{ evento.sede }}</label>
+      </v-row>
+      <v-row>
+        <v-col>
+          <label><strong>Cupo del evento:</strong></label>
+        </v-col>
+        <v-col>
+          <label> {{ evento.cupo }}</label>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <label><strong>Descripción: </strong></label>
+        <v-spacer></v-spacer>
+        <label>{{ evento.descripcion }}</label>
+      </v-row>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        block
+        depressed
+        color="error"
+        @click="sendEvent(evento)"
+      >
+        Registro y Asistencia
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+  <v-card v-else>
+    <v-card-text>
+      <v-row>
+        <h4>Favor de seleccionar un evento para mas detalles</h4>
+      </v-row>
+      <v-row class="center">
+        <v-btn
+          loading
+          text
+          x-large
+        ></v-btn>
+      </v-row>
+      
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        block
+        depressed
+        disabled
+        color="error"
+      >
+        Registro y Asistencia
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+  <!-- <v-row v-else>
+    <div>
+        <p>Por favor selecciona un Evento para ver más información del mismo</p>
+    </div>
+  </v-row> -->
+</template>
+
+<script>
+export default {
+    name: 'info-Evento',
+    data() {
+      return {
+
+      };
+    },
+    props: {
+        evento: {
+            type: Object,
+        }
+    },
+    methods: {
+      sendEvent(evento) {
+        console.log(evento)
+        this.$router.push("/fi-registro/"+evento.id);
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- projectpfi/pfi-front/src/components/FormacionIntegral/FormacionIntegral.vue
+++ projectpfi/pfi-front/src/components/FormacionIntegral/FormacionIntegral.vue
@@ -1,50 +1,51 @@
 <template>
   <v-container>
-    <v-data-table
-      v-model="selected"
-      :headers="headers"
-      :items="eventos"
-      :single-select="singleSelect"
-      :single-expand="singleExpand"
-      :expanded.sync="expanded"
-      :search="search"
-      item-key="id"
-      show-select
-      show-expand
-      class="elevation-1"
-    >
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title>Eventos</v-toolbar-title>
-          <v-spacer></v-spacer>
-            <v-text-field
-              v-model="search"
-              append-icon="mdi-magnify"
-              label="Buscar"
-              single-line
-              hide-details
-            ></v-text-field>
-        </v-toolbar>
-      </template>
-      <template v-slot:expanded-item="{ headers, item }">
-        <td :colspan="headers.length">
-          {{ item.descripcionEvento }}
-        </td>
-      </template>
-    </v-data-table>
     <v-row>
-      <v-btn
-        depressed
-        elevation="2"
-        @click="sendEvent(selected)"
-      >Registro y Asistencia</v-btn>
+      <v-col>
+        <v-data-table
+          v-model="selected"
+          :headers="headers"
+          :items="eventos"
+          :single-select="singleSelect"
+          :search="search"
+          item-key="id"
+          show-select
+          class="elevation-1"
+        >
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>Eventos</v-toolbar-title>
+              <v-spacer></v-spacer>
+                <v-text-field
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="Buscar"
+                  single-line
+                  hide-details
+                ></v-text-field>
+            </v-toolbar>
+          </template>
+        </v-data-table>
+        <!-- <v-row>
+          <v-btn
+            depressed
+            elevation="2"
+            @click="sendEvent(selected)"
+          >Registro y Asistencia</v-btn>
+        </v-row> -->
+      </v-col>
+      <v-col>
+        <info-Evento :evento = "selected[0]"></info-Evento>
+      </v-col>
     </v-row>
+    
     
   </v-container>
 </template>
 
 <script>
 import EventosDataService from "../../services/EventosDataService";
+import infoEvento from "../Eventos/InfoEvento";
 
 export default {
     name: "formacionIntegral",
@@ -52,7 +53,22 @@ export default {
       return {
         eventos: [],
         singleSelect: true,
-        selected: [],
+        selected: [
+          {
+            tituloEvento: '',
+            unidadResponsable: '',
+            descripcionEvento: '',
+            eventoDedicadoA:'',
+            fechaEvento:'',
+            inicioEvento:'',
+            finEvento:'',
+            sede:'',
+            cupo:'',
+            descripcion:'',
+            creditos:'',
+            categorias:'',
+          }
+        ],
         singleExpand: true,
         expanded: [],
         search: '',
@@ -66,6 +82,9 @@ export default {
         ],
       };
     },
+    components: {
+      infoEvento
+    },
     methods: {
       retrieveEventos() {
         EventosDataService.getAll()
@@ -77,16 +96,17 @@ export default {
             console.log(e);
           });
       },
-      sendEvent(evento) {
+/*       sendEvent(evento) {
         console.log(evento)
-        this.$router.push("/fi-registro/"+evento[0].id);
-      }
+        this.$router.push("/fi-registro/"+evento.id);
+      } */
     },
     mounted() {
       
     },
     created(){
       this.retrieveEventos();
+      console.log(this.ejemplo)
     }
 }
 </script>
